d636eff6b7db3ef2e2f9473eef9eec791cad6566
rehabmalik <144557438+rehabmalik@users.noreply.github.com>
2023-10-14 14:29:00 -0400

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,8 +1,43 @@
 public class Passenger {
-    
-    private String name;
+    String name;
 
-    public Passenger(String name) {
+    /**
+     * Creates an instance of the train class and initiliases the attributes
+     *@param name String
+    */
+    public Passenger(String name){
         this.name = name;
+    }
+
+    /**
+     * Allows access to the passenger's name
+     * @return name
+    */
+    public String getName(){
+        return name;
+    }
+    
+    /**
+     * Adds passenger to a given car
+     * @param c Car
+    */
+    public void boardCar(Car c){
+        try {
+            c.addPassenger(this);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Removes passemger from given car
+     * @param c Car
+    */
+    public void getOffCar(Car c) {
+        try {
+            c.removePassenger(this);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
