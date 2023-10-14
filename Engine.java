1b3999bf5132ef07cc9c28104362c92f1342b1d6
rehabmalik <144557438+rehabmalik@users.noreply.github.com>
2023-10-14 14:48:55 -0400

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -7,40 +7,46 @@ public class Engine {
      * Creates a new engine and initializes its attributes
      * @param currentfuellevel
      * @param fueltype
-    */
-   public Engine (double currentfuellevel, FuelType fueltype){
+     */
+   
+    public Engine (double currentfuellevel, FuelType fueltype){
      this.currentfuellevel = currentfuellevel;
      this.maxfuellevel = 100.0;
      this.fueltype = fueltype;
     }
+    
     /**
      * Gives the current fuel level of the engine
      * @return currentfuellevel
-    */
-    public double getCurrentFuelLevel() {
+     */
+    
+     public double getCurrentFuelLevel() {
         return currentfuellevel;
     }
 
     /**
      * Gives the maximum fuel level of the engine
      * @return maxfuellevel
-    */
+     */
+    
     public double getMaxFuelLevel() {
         return maxfuellevel;
     }
 
     /**
      * Refuels the engine to its maximum level
-    */
+     */
+    
     public void refuel() {
         currentfuellevel = maxfuellevel;
     }
-    
+
    /**
       * Decreases the fuel level according to the time travelled and prints remaining fuel
       * The train can travel for 24 hours before it needs to be refuelled again. 
       * @param traveltime
-    */
+     */
+
     public void go(double traveltime) {
         double fuelconsumed;
        currentfuellevel = getCurrentFuelLevel();
