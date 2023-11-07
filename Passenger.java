eff456c8e3a7dd4313ded708542338a3fcfccbd2
rehabmalik <144557438+rehabmalik@users.noreply.github.com>
2023-11-07 14:27:46 -0500

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,77 +1,47 @@
-import java.util.ArrayList;
+public class Passenger {
+    String name;
 
-public class Train {
-    private final Engine engine;
-    private ArrayList<Car> cars;
-   
     /**
-     * creates an instance of the train class and initiliases the attributes
-     *@param fuelType fuelType
-     *@param fuelCapacity double
-     *@param nCars int
-     *@param passengerCapacity int
+     * Creates an instance of the train class and initiliases the attributes
+     *@param name String
      */
 
-    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity){
-        this.cars = new ArrayList<>(nCars);
-        this.engine = new Engine(fuelCapacity, fuelType);
-        for (int i = 1; i <= nCars; i ++){
-            this.cars.add(new Car(passengerCapacity));
+    public Passenger(String name){
+        this.name = name;
+    }
+
+    /**
+     * Allows access to the passenger's name
+     * @return name
+     */
+    
+     public String getName(){
+        return name;
+    }
+    
+    /**
+     * Adds passenger to a given car
+     * @param c Car
+     */
+    
+    public void boardCar(Car c){
+        try {
+            c.addPassenger(this);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
     }
 
     /**
-     * Gives access to the engine linked to this train
-     *@return the engine linked to this train
+     * Removes passemger from given car
+     * @param c Car
      */
     
-    public Engine getEngine(){
-        return this.engine;
-    } 
-
-    /** 
-     *Searches the array for the car stored at the requested index
-     *@return the car stored at index i in the array list
-     */
-    
-    public Car getCar(int i){
-        return this.cars.get(i);
-    }
-
-    /**
-     * Totals the capacities of all cars in the train
-     *@return maxcapacity
-     */
-    
-    public int getMaxCapacity(){
-        int maxcapacity = 0;
-        for (Car car : cars) {
-            maxcapacity = maxcapacity + car.getCapacity();
-        }
-        return maxcapacity;
-    }
-
-    /**
-     * Totals the remaining seats in the cars of the train
-     *@return seatsremaining
-     */
-    
-    public int seatsRemaining(){
-        int seatsremaining = 0;
-        for (Car car : cars) {
-            seatsremaining = seatsremaining + car.seatsRemaining();
-        }
-        return seatsremaining;
-    }
-
-    /**
-     * Prints all the passengers aboard the train organized by car.
-     */
-    
-    public void printManifest(){
-        for (Car car : cars) {
-            System.out.println("Car " + (this.cars.indexOf(car)+ 1));
-            car.printManifest();
+    public void getOffCar(Car c) {
+        try {
+            c.removePassenger(this);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
     }
 }
